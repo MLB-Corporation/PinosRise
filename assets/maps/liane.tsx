65cb722cb531f9f55329ceea88797f04aeb2c967
Bugni Federico <122440144+FedeBugni@users.noreply.github.com>
2024-04-05 15:46:08 +0200

    added right maps files

---
+++ assets/maps/liane.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="liane" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <grid orientation="orthogonal" width="8" height="8"/>
+ <image source="VinesSheet.png" width="281" height="131"/>
+</tileset>
